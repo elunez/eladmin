3cde757b32bdd5e4092ba6c11dfa7b89a319c166
zhengjie <zhengjie@tom.com>
2019-05-08 15:25:27 +0800

    打包时跳过单元测试

--- eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
+++ eladmin-system/src/test/java/me/zhengjie/EladminSystemApplicationTests.java
@@ -1,9 +1,7 @@
 package me.zhengjie;
 
-import me.zhengjie.modules.monitor.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
