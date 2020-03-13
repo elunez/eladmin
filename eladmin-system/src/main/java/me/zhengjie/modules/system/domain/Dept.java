7c81232e637e0ccd4267183bbc935aec3b168abd
wtune <59522871+wtune@users.noreply.github.com>
2020-03-13 16:12:50 +0800

    Add Index on Dept.pid in Dept.java

--- eladmin-system/src/main/java/me/zhengjie/modules/system/domain/Dept.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/domain/Dept.java
@@ -19,7 +19,8 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@Table(name="dept")
+@Table(name="dept",
+      indexs={@Index(name="idx_pid",columnList="pid")})
 public class Dept implements Serializable {
 
     @Id
