921c462b1268a342583eac8e1275a3ad7ae8d20d
ZhengJie <201507802@qq.com>
2020-05-07 19:05:24 +0800

    [代码完善](v2.5): v2.5 beta 数据权限使升级，现可通过注解[@DataPermission]控制
    DataPermission 类中有详细说明和使用示例
    
    SecurityUtils 中加入获取当前用户的数据权限的方法
    
    2.5 Beta 详情：https://www.ydyno.com/archives/1225.html

---
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/DataServiceImpl.java
@@ -0,0 +1,104 @@
+/*
+ *  Copyright 2019-2020 Zheng Jie
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package me.zhengjie.modules.system.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.zhengjie.modules.system.domain.Dept;
+import me.zhengjie.modules.system.service.DataService;
+import me.zhengjie.modules.system.service.DeptService;
+import me.zhengjie.modules.system.service.RoleService;
+import me.zhengjie.modules.system.service.dto.RoleSmallDto;
+import me.zhengjie.modules.system.service.dto.UserDto;
+import me.zhengjie.utils.enums.DataScopeEnum;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+/**
+ * @author Zheng Jie
+ * @website https://docs.auauz.net
+ * @description 数据权限服务实现
+ * @date 2020-05-07
+ **/
+@Service
+@RequiredArgsConstructor
+@CacheConfig(cacheNames = "role")
+public class DataServiceImpl implements DataService {
+
+    private final RoleService roleService;
+    private final DeptService deptService;
+
+    @Cacheable
+    public List<Long> getDeptIds(UserDto user) {
+        // 用于存储部门id
+        Set<Long> deptIds = new HashSet<>();
+        // 查询用户角色
+        List<RoleSmallDto> roleSet = roleService.findByUsersId(user.getId());
+        // 获取对应的部门ID
+        for (RoleSmallDto role : roleSet) {
+            DataScopeEnum dataScopeEnum = DataScopeEnum.find(role.getDataScope());
+            switch (Objects.requireNonNull(dataScopeEnum)) {
+                case THIS_LEVEL:
+                    deptIds.add(user.getDept().getId());
+                    break;
+                case CUSTOMIZE:
+                    deptIds.addAll(getCustomize(deptIds, role));
+                    break;
+            }
+        }
+        return new ArrayList<>(deptIds);
+    }
+
+    /**
+     * 获取自定义的数据权限
+     * @param deptIds 部门ID
+     * @param role 角色
+     * @return 数据权限ID
+     */
+    public Set<Long> getCustomize(Set<Long> deptIds, RoleSmallDto role){
+        Set<Dept> depts = deptService.findByRoleIds(role.getId());
+        for (Dept dept : depts) {
+            deptIds.add(dept.getId());
+            List<Dept> deptChildren = deptService.findByPid(dept.getId());
+            if (deptChildren != null && deptChildren.size() != 0) {
+                deptIds.addAll(getDeptChildren(deptChildren));
+            }
+        }
+        return deptIds;
+    }
+
+    /**
+     * 递归获取子级部门
+     * @param deptList 部门
+     * @return 数据权限
+     */
+    @Cacheable
+    public List<Long> getDeptChildren(List<Dept> deptList) {
+        List<Long> list = new ArrayList<>();
+        deptList.forEach(dept -> {
+                    if (dept!=null && dept.getEnabled()){
+                        List<Dept> depts = deptService.findByPid(dept.getId());
+                        if(deptList.size() != 0){
+                            list.addAll(getDeptChildren(depts));
+                        }
+                        list.add(dept.getId());
+                    }
+                }
+        );
+        return list;
+    }
+}
