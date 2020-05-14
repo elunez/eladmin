2d969b43a5841b6b92a393e87723c74bb7924c0a
ZhengJie <201507802@qq.com>
2020-05-14 14:07:25 +0800

    [代码完善](v2.5): v2.5 beta 审计获取当前操作人优化
    2.5 Beta 详情：https://www.ydyno.com/archives/1225.html

--- eladmin-common/src/main/java/me/zhengjie/config/AuditorConfig.java
+++ eladmin-common/src/main/java/me/zhengjie/config/AuditorConfig.java
@@ -35,7 +35,11 @@ public class AuditorConfig implements AuditorAware<String> {
      */
     @Override
     public Optional<String> getCurrentAuditor() {
-        // 这里应根据实际业务情况获取具体信息
-        return Optional.of(SecurityUtils.getCurrentUsername());
+        try {
+            // 这里应根据实际业务情况获取具体信息
+            return Optional.of(SecurityUtils.getCurrentUsername());
+        }catch (Exception ignored){}
+        // 用户定时任务，或者无Token调用的情况
+        return Optional.of("System");
     }
 }
