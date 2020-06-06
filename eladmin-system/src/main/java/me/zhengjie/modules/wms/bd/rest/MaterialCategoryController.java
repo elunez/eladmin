156efd7a88c599f5e47a0bcb65a18b69ae41c212
zhangmingming <738993102@qq.com>
2020-06-06 12:00:21 +0800

    .

--- eladmin-system/src/main/java/me/zhengjie/modules/wms/bd/rest/MaterialCategoryController.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/wms/bd/rest/MaterialCategoryController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api")
 public class MaterialCategoryController {
 
+
     @Autowired
     private MaterialCategoryService materialCategoryService;
 
