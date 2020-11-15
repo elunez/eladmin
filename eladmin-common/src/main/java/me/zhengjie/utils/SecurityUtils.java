11bf58688a24e32757dc261a7ddba6ae7a9ce7d7
wuyc <40560751+wuyc@users.noreply.github.com>
2020-11-15 19:37:54 +0800

    [代码优化](v2.6): 代码优化 (#526)

--- eladmin-common/src/main/java/me/zhengjie/utils/SecurityUtils.java
+++ eladmin-common/src/main/java/me/zhengjie/utils/SecurityUtils.java
@@ -41,16 +41,8 @@ public class SecurityUtils {
      * @return UserDetails
      */
     public static UserDetails getCurrentUser() {
-        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null) {
-            throw new BadRequestException(HttpStatus.UNAUTHORIZED, "当前登录状态过期");
-        }
-        if (authentication.getPrincipal() instanceof UserDetails) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            UserDetailsService userDetailsService = SpringContextHolder.getBean(UserDetailsService.class);
-            return userDetailsService.loadUserByUsername(userDetails.getUsername());
-        }
-        throw new BadRequestException(HttpStatus.UNAUTHORIZED, "找不到当前登录的信息");
+        UserDetailsService userDetailsService = SpringContextHolder.getBean(UserDetailsService.class);
+        return userDetailsService.loadUserByUsername(getCurrentUsername());
     }
 
     /**
@@ -63,8 +55,11 @@ public class SecurityUtils {
         if (authentication == null) {
             throw new BadRequestException(HttpStatus.UNAUTHORIZED, "当前登录状态过期");
         }
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return userDetails.getUsername();
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return userDetails.getUsername();
+        }
+        throw new BadRequestException(HttpStatus.UNAUTHORIZED, "找不到当前登录的信息");
     }
 
     /**
