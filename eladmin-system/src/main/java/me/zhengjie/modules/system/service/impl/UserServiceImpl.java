a90bfac2cf3324ca47f155e6a90a02280fc7fcd9
zhengjie <201507802@qq.com>
2020-12-24 16:45:08 +0800

    [代码优化](v2.6)：禁用用户后踢出用户Bug修复
    close https://github.com/elunez/eladmin/issues/551

--- eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/UserServiceImpl.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/UserServiceImpl.java
@@ -98,7 +98,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public void update(User resources) {
+    public void update(User resources) throws Exception {
         User user = userRepository.findById(resources.getId()).orElseGet(User::new);
         ValidationUtil.isNull(user.getId(), "User", "id", resources.getId());
         User user1 = userRepository.findByUsername(resources.getUsername());
