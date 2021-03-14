3b802fd770bbc7e25438e185e0398ec3232a3418
ice_cola <53242048+pangyv@users.noreply.github.com>
2021-03-14 12:46:37 +0800

    fix:A custom permission role does not refresh the DataScope cache after adding subdepartments to a department (#608)

--- eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/DeptServiceImpl.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/impl/DeptServiceImpl.java
@@ -113,6 +113,8 @@ public class DeptServiceImpl implements DeptService {
         resources.setSubCount(0);
         // 清理缓存
         updateSubCnt(resources.getPid());
+        // 清理自定义角色权限的datascope缓存
+        delCaches(resources.getPid());
     }
 
     @Override
