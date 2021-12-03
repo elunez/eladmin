77a77933f1a02fc33dfdace4fff3d165d9d4e528
Night_mare <55904958+chong-chonga@users.noreply.github.com>
2021-12-03 15:57:49 +0800

    删除不用的包
    改用 org.apache.commons.lang3.StringUtils 全类名调用 isBlank() 方法后, 原 import me.zhengjie.utils.StringUtils; 可以删除

--- eladmin-common/src/main/java/me/zhengjie/config/RedisConfig.java
+++ eladmin-common/src/main/java/me/zhengjie/config/RedisConfig.java
@@ -20,7 +20,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
-import me.zhengjie.utils.StringUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
