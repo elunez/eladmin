d78e1decb3830b88fc64859b9baa10563cb9de82
Zheng Jie <201507802@qq.com>
2022-05-24 16:34:47 +0800

    新增 AuthorityDto 类，避免 GrantedAuthority 序列化报错问题

--- eladmin-system/src/main/java/me/zhengjie/modules/system/service/RoleService.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/system/service/RoleService.java
@@ -15,13 +15,13 @@
  */
 package me.zhengjie.modules.system.service;
 
+import me.zhengjie.modules.security.service.dto.AuthorityDto;
 import me.zhengjie.modules.system.domain.Role;
 import me.zhengjie.modules.system.service.dto.RoleDto;
 import me.zhengjie.modules.system.service.dto.RoleQueryCriteria;
 import me.zhengjie.modules.system.service.dto.RoleSmallDto;
 import me.zhengjie.modules.system.service.dto.UserDto;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.GrantedAuthority;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
@@ -119,7 +119,7 @@ public interface RoleService {
      * @param user 用户信息
      * @return 权限信息
      */
-    List<GrantedAuthority> mapToGrantedAuthorities(UserDto user);
+    List<AuthorityDto> mapToGrantedAuthorities(UserDto user);
 
     /**
      * 验证是否被用户关联
