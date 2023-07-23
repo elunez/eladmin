19f71cfa525546cd198295eef769405def2c7d28
Your Name <you@example.com>
2023-07-22 23:58:25 -0500

    Add Actuator Health Checking

--- eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
+++ eladmin-system/src/main/java/me/zhengjie/config/ConfigurerAdapter.java
@@ -18,10 +18,27 @@ package me.zhengjie.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.EndpointLinksResolver;
+import org.springframework.boot.actuate.endpoint.web.EndpointMapping;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.ExposableWebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -30,6 +47,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -40,6 +58,7 @@ import java.util.List;
  */
 @Configuration
 @EnableWebMvc
+@EnableSwagger2
 public class ConfigurerAdapter implements WebMvcConfigurer {
 
     /** 文件配置 */
@@ -84,5 +103,50 @@ public class ConfigurerAdapter implements WebMvcConfigurer {
         converter.setSupportedMediaTypes(supportMediaTypeList);
         converter.setDefaultCharset(StandardCharsets.UTF_8);
         converters.add(converter);
+    }
+
+
+    /**
+    * 注册endpoints,解决springboot升级到2.6.x之后，actuator error problems
+    *
+    * @param webEndpointsSupplier        the web endpoints supplier
+    * @param servletEndpointsSupplier    the servlet endpoints supplier
+    * @param controllerEndpointsSupplier the controller endpoints supplier
+    * @param endpointMediaTypes          the endpoint media types
+    * @param corsProperties              the cors properties
+    * @param webEndpointProperties       the web endpoint properties
+    * @param environment                 the environment
+    * @return the web mvc endpoint handler mapping
+    */
+    @Bean
+    public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier, ServletEndpointsSupplier servletEndpointsSupplier,
+    ControllerEndpointsSupplier controllerEndpointsSupplier, EndpointMediaTypes endpointMediaTypes,
+    CorsEndpointProperties corsProperties, WebEndpointProperties webEndpointProperties,
+    Environment environment) {
+        List<ExposableEndpoint<?>> allEndpoints = new ArrayList<>();
+        Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+        allEndpoints.addAll(webEndpoints);
+        allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+        allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+        String basePath = webEndpointProperties.getBasePath();
+        EndpointMapping endpointMapping = new EndpointMapping(basePath);
+        boolean shouldRegisterLinksMapping = shouldRegisterLinksMapping(webEndpointProperties, environment, basePath);
+
+    return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+      corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+      shouldRegisterLinksMapping);
+    }
+    
+    /**
+    * shouldRegisterLinksMapping
+    * @param webEndpointProperties webEndpointProperties
+    * @param environment environment
+    * @param basePath /
+    * @return boolean
+    */
+    private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties,
+    Environment environment, String basePath) {
+    return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath)
+      || ManagementPortType.get(environment).equals(ManagementPortType.DIFFERENT));
     }
 }
