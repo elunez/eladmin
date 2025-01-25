61f8dc32cf80e1091fe6c046783a0ccadb056da7
Jie Zheng <201507802@qq.com>
2025-01-25 13:43:20 +0800

    feat: 优化Redis配置，添加AutoConfigureBefore注解

--- eladmin-common/src/main/java/me/zhengjie/config/RedissonConfiguration.java
+++ eladmin-common/src/main/java/me/zhengjie/config/RedissonConfiguration.java
@@ -21,11 +21,14 @@ import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
+@AutoConfigureBefore(RedisAutoConfiguration.class)
 public class RedissonConfiguration {
 
     @Value("${spring.redis.host}")
