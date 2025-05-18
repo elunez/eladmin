574cfb3ac0ee868e23a3f9c5f3ac0715bd256b3b
chanhengseang <chanheng.seang@mbanq.com>
2025-05-18 14:44:35 -0700

    add event

--- sport/src/main/java/com/srr/domain/Event.java
+++ sport/src/main/java/com/srr/domain/Event.java
@@ -43,7 +43,7 @@ public class Event implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "`id`")
-    @ApiModelProperty(value = "id")
+    @ApiModelProperty(value = "id", hidden = true)
     private Long id;
 
     @Column(name = "`name`",nullable = false)
@@ -75,12 +75,12 @@ public class Event implements Serializable {
 
     @Column(name = "`create_time`")
     @CreationTimestamp
-    @ApiModelProperty(value = "创建时间")
+    @ApiModelProperty(value = "创建时间", hidden = true)
     private Timestamp createTime;
 
     @Column(name = "`update_time`")
     @UpdateTimestamp
-    @ApiModelProperty(value = "更新时间")
+    @ApiModelProperty(value = "更新时间", hidden = true)
     private Timestamp updateTime;
 
     @Column(name = "`sort`")
